b3bcb61ca7ebcbc26f6b9ba236177d4b4641fff5
tusyuno <madokawaii@gmail.com>
2017-02-24 14:45:32 +0900

    Use some objects to improve readavility.

--- src/javascripts/modules/ExtendedBar.js
+++ src/javascripts/modules/ExtendedBar.js
@@ -20,32 +20,42 @@ function put(idName) {
 
 function setUp() {
   Api.getStatus(idHolder.liveId).then((response) => {
-    const currentTime = Date.now();
-    const currentDate = new Date(currentTime);
-    // new Date() は引数にミリ秒を要求するので 1000 倍するために末尾に '000' を付加する．
-    const endTime = Number(`${$(response).find('stream end_time').text()}000`);
-    const endDate = new Date(endTime);
-    const endTimeJpn = Time.toJpnString(endDate.getTime());
-    const restTime_Hour = Time.hourDistance(currentDate, endDate);
-    let restTime_Minute = Time.minuteSurplusDistance(currentDate, endDate);
-    let restTime_Second = Time.secondSurplusDistance(currentDate, endDate);
-    if (Number(restTime_Hour) > 0) {
-      restTime_Minute = `0${restTime_Minute}`.slice(-2);
+    const endTimes = {
+      sec: '',
+      millisec: '',
+      jpstr: ''
+    };
+    endTimes.sec = Number($(response).find('stream end_time').text());
+    endTimes.millisec = Number(`${endTimes.sec}000`); // new Date() requires millisecond.
+    const dates = {
+      now: '',
+      end: ''
+    };
+    dates.now = new Date(Date.now());
+    dates.end = new Date(endTimes.millisec);
+    endTimes.jpstr = Time.toJpnString(dates.end.getTime());
+    const remainingTimes = {
+      hour: '',
+      min: '',
+      sec: ''
+    };
+    remainingTimes.hour = Time.hourDistance(dates.now, dates.end);
+    remainingTimes.min = Time.minuteSurplusDistance(dates.now, dates.end);
+    remainingTimes.sec = Time.secondSurplusDistance(dates.now, dates.end);
+    if (Number(remainingTimes.hour) > 0) {
+      remainingTimes.min = `0${remainingTimes.min}`.slice(-2);
     }
-    restTime_Second = `0${restTime_Second}`.slice(-2);
-    /**
-     * Initialize ExtendedBar's timer.
-     */
-    timeCounter.setHour(restTime_Hour);
-    timeCounter.setMinute(restTime_Minute);
-    timeCounter.setSecond(restTime_Second);
+    remainingTimes.sec = `0${remainingTimes.sec}`.slice(-2);
+    timeCounter.setHour(remainingTimes.hour);
+    timeCounter.setMinute(remainingTimes.min);
+    timeCounter.setSecond(remainingTimes.sec);
 
-    $('#extended-bar .end-time').text(`${endTimeJpn}`);
-    if (Number(restTime_Hour) > 0) {
-      $('#extended-bar .rest-time').text(`${restTime_Hour}：${restTime_Minute}：${restTime_Second}`);
+    if (Number(remainingTimes.hour) > 0) {
+      $('#extended-bar .rest-time').text(`${remainingTimes.hour}：${remainingTimes.min}：${remainingTimes.sec}`);
     } else {
-      $('#extended-bar .rest-time').text(`${restTime_Minute}：${restTime_Second}`);
+      $('#extended-bar .rest-time').text(`${remainingTimes.min}：${remainingTimes.sec}`);
     }
+    $('#extended-bar .end-time').text(`${endTimes.jpstr}`);
   });
 }
 
