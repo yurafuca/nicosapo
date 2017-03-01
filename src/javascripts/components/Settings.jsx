3fa30203eff88547515a55331ceb327e53e1b9e2
tusyuno <madokawaii@gmail.com>
2017-03-01 14:35:37 +0900

    Change style of option page

--- src/javascripts/components/Settings.jsx
+++ src/javascripts/components/Settings.jsx
@@ -19,14 +19,15 @@ export default class Settings extends React.Component {
     this.onChangeCheckBox = this.onChangeCheckBox.bind(this);
     this.reflectSettings  = this.reflectSettings.bind(this);
     this.playSound        = this.playSound.bind(this);
+    this.clickMenu        = this.clickMenu.bind(this);
   }
 
   getInitialState() {
     const state = {
+      selectedMenu: 'basic',
       resultMessage: '',
       selectableList: {
         minuteList: [15, 20, 25, 30, 25, 40, 50, 60, 70, 80, 90, 100, 110, 120],
-        toastTimes: [1, 2, 3, 4, 5],
         soundFiles: [
           {path: 'ta-da.mp3',   text: 'Ta-da!'},
           {path: 'ding.mp3',    text: 'Ding'},
@@ -36,8 +37,7 @@ export default class Settings extends React.Component {
           {path: 'famima.mp3',  text: 'ファミマ'},
         ]
       },
-      'options.redirect.time':                50,
-      'options.toast.minuteList':             [1, 3, 5],
+      'options.redirect.time':                30,
       'options.soundfile':                    'ta-da.mp3',
       'options.autoJump.enable':              'enable',
       'options.showReserved.enable':          'enable',
@@ -50,14 +50,14 @@ export default class Settings extends React.Component {
   }
 
   componentDidMount() {
-    ReactDOM.render(
-      <AutoEnterList type='community' />,
-      document.getElementById('listgroup-community')
-    );
-    ReactDOM.render(
-      <AutoEnterList type='program' />,
-      document.getElementById('listgroup-program')
-    );
+    // ReactDOM.render(
+    //   <AutoEnterList type='community' />,
+    //   document.getElementById('listgroup-community')
+    // );
+    // ReactDOM.render(
+    //   <AutoEnterList type='program' />,
+    //   document.getElementById('listgroup-program')
+    // );
     this.reflectSettings();
   }
 
@@ -108,91 +108,124 @@ export default class Settings extends React.Component {
     });
   }
 
+  clickMenu(e) {
+    console.info(e.target.dataset.menu);
+    this.setState({ selectedMenu: e.target.dataset.menu});
+  }
+
   render() {
     return (
       <div>
         <div className="content">
-          <div className="wrapper">
-            <h1 className="appicon">基本設定</h1>
-            <div className="items">
-              <div>
-                <h3>自動次枠移動の更新間隔</h3>
-                <select  name="options.redirect.time"  onChange={this.onChange} value={this.state['options.redirect.time']}>
-                  {this.state.selectableList.minuteList.map((d) =>
-                    <option value={d}>
-                      {d}秒
-                    </option>
-                  )}
-                </select>
-                <p className='note red'> 音声や動画が頻繁に停止する場合は時間を長めに設定してください </p>
-              </div>
-              <div>
-                <h3>自動次枠移動をデフォルトで「ON」にする</h3>
-                <input type="radio" name="options.autoJump.enable" value={'enable'} checked={this.state['options.autoJump.enable'] == 'enable'} onChange={this.onChange} /> 有効
-                <input type="radio" name="options.autoJump.enable" value={'disable'} checked={this.state['options.autoJump.enable'] == 'disable'} onChange={this.onChange} /> 無効
-              </div>
-              <div>
-                <h3>予約番組をポップアップに表示する</h3>
-                <input type="radio" name="options.showReserved.enable" value={'enable'} checked={this.state['options.showReserved.enable'] == 'enable'} onChange={this.onChange} /> 有効
-                <input type="radio" name="options.showReserved.enable" value={'disable'} checked={this.state['options.showReserved.enable'] == 'disable'} onChange={this.onChange} /> 無効
-              </div>
-              <div>
-                <h3>通知を放送開始時に表示する</h3>
-                <input type="radio" name="options.popup.enable" value={'enable'} checked={this.state['options.popup.enable'] == 'enable'} onChange={this.onChange} /> 有効
-                <input type="radio" name="options.popup.enable" value={'disable'} checked={this.state['options.popup.enable'] == 'disable'} onChange={this.onChange} /> 無効
-              </div>
-              <div>
-                <h3>音を放送開始時に鳴らす</h3>
-                <input type="radio" name="options.playsound.enable" value={'enable'} checked={this.state['options.playsound.enable'] == 'enable'} onChange={this.onChange} /> 有効
-                <input type="radio" name="options.playsound.enable" value={'disable'} checked={this.state['options.playsound.enable'] == 'disable'} onChange={this.onChange} /> 無効
-              </div>
-              <div>
-                <h3>通知の表示位置</h3>
-                <p style={{color:'#767676'}}>Google Chrome の仕様上変更できません．</p>
-              </div>
-              <div>
-                <h3>「放送開始のお知らせ」通知の表示時間</h3>
-                <input type="range" name="options.openingNotification.duration" value={this.state['options.openingNotification.duration']} min="0" max="20" step="1" onChange={this.onChange} />
-                <span id="openingNotification.duration">{this.state['options.openingNotification.duration']}秒</span>
-              </div>
-              <div>
-                <h3>通知音</h3>
-                <select name="options.soundfile" onChange={this.onChange} value={this.state['options.soundfile']}>
-                  {this.state.selectableList.soundFiles.map((d) =>
-                    <option value={d.path}>{d.text}</option>
-                  )}
-                </select>
-                <p className='note'>選択するとサンプル音が鳴ります</p>
-              </div>
-              <div>
-                <h3>通知音のボリューム</h3>
-                <input type="range" name="options.playsound.volume" value={this.state['options.playsound.volume']} min="0.0" max="1.0" step="0.1" onChange={this.onChange} />
-                <button className="soundtest" onClick={this.playSound}>音量テスト</button>
-              </div>
-              <div id="buttons">
-                <input id="saveAll" type="submit" value="設定を保存する" onClick={this.saveSettings}/>
-                <p id="console" style={{color: '#228b22'}}>{this.state.resultMessage}</p>
-              </div>
+          <div>
+            <div className="wrapper menu float-left">
+              <h1 className="appicon">設定項目</h1>
+              <div className={this.state.selectedMenu === 'basic' ? 'item selected' : 'item'} data-menu="basic" onClick={this.clickMenu}>基本設定</div>
+              <div className={this.state.selectedMenu === 'auto-program' ? 'item selected' : 'item'} data-menu="auto-program" onClick={this.clickMenu}>自動枠移動リスト（番組）</div>
+              <div className={this.state.selectedMenu === 'auto-community' ? 'item selected' : 'item'} data-menu="auto-community" onClick={this.clickMenu}>自動枠移動リスト（CH・コミュ）</div>
             </div>
           </div>
-          <div className="wrapper">
-            <h1 className="appicon">自動入場が有効になっている番組</h1>
-            <div id="listgroup-program">
-            </div>
-          </div>
-          <div className="wrapper">
-            <h1 className="appicon">自動入場が有効になっているコミュニティ/チャンネル</h1>
-            <div id="listgroup-community">
-            </div>
-          </div>
-          <div className="wrapper">
-            <h1 className="appicon">作者にカンパする</h1>
-            <div className="items">
-              <span className="campa">にこさぽの開発を応援してくださる方はよろしくお願いします．今後の開発の励みになります．🙏</span>
-                <p> <a target="_blank" href="http://amzn.asia/hqChgj3">Amazon ほしいものリスト - ほしい雑貨</a></p>
-                <p> <a target="_blank" href="http://amzn.asia/8BFBccC">Amazon ほしいものリスト - ほしい本</a></p>
-            </div>
-          </div>
+          {(() => {
+            if (this.state.selectedMenu == 'basic') {
+              return(
+                <div className="wrapper">
+                  <h1 className="appicon">基本設定</h1>
+                  <div className="items">
+                    <div className="item">
+                      <h3>自動次枠移動の更新間隔</h3>
+                      <select name="options.redirect.time"  onChange={this.onChange} value={this.state['options.redirect.time']}>
+                        {this.state.selectableList.minuteList.map((d) =>
+                          <option value={d}>
+                            {d}秒
+                          </option>
+                        )}
+                      </select>
+                      <p className='note red'> 音声や動画が頻繁に停止する場合は時間を長めに設定してください </p>
+                    </div>
+                    <div className="item">
+                      <h3>自動次枠移動をデフォルトで「ON」にする</h3>
+                      <input type="radio" name="options.autoJump.enable" value={'enable'} checked={this.state['options.autoJump.enable'] == 'enable'} onChange={this.onChange} /> 有効
+                      <input type="radio" name="options.autoJump.enable" value={'disable'} checked={this.state['options.autoJump.enable'] == 'disable'} onChange={this.onChange} /> 無効
+                    </div>
+                    <div className="item">
+                      <h3>予約番組をポップアップに表示する</h3>
+                      <input type="radio" name="options.showReserved.enable" value={'enable'} checked={this.state['options.showReserved.enable'] == 'enable'} onChange={this.onChange} /> 有効
+                      <input type="radio" name="options.showReserved.enable" value={'disable'} checked={this.state['options.showReserved.enable'] == 'disable'} onChange={this.onChange} /> 無効
+                    </div>
+                    <div className="item">
+                      <h3>通知を放送開始時に表示する</h3>
+                      <input type="radio" name="options.popup.enable" value={'enable'} checked={this.state['options.popup.enable'] == 'enable'} onChange={this.onChange} /> 有効
+                      <input type="radio" name="options.popup.enable" value={'disable'} checked={this.state['options.popup.enable'] == 'disable'} onChange={this.onChange} /> 無効
+                    </div>
+                    <div className="item">
+                      <h3>音を放送開始時に鳴らす</h3>
+                      <input type="radio" name="options.playsound.enable" value={'enable'} checked={this.state['options.playsound.enable'] == 'enable'} onChange={this.onChange} /> 有効
+                      <input type="radio" name="options.playsound.enable" value={'disable'} checked={this.state['options.playsound.enable'] == 'disable'} onChange={this.onChange} /> 無効
+                    </div>
+                    <div className="item">
+                      <h3>通知の表示位置</h3>
+                      <p style={{color:'#767676'}}>Google Chrome の仕様上変更できません．</p>
+                    </div>
+                    <div className="item">
+                      <h3>「放送開始のお知らせ」通知の表示時間</h3>
+                      <input type="range" name="options.openingNotification.duration" value={this.state['options.openingNotification.duration']} min="0" max="20" step="1" onChange={this.onChange} />
+                      <span id="openingNotification.duration">{this.state['options.openingNotification.duration']}秒</span>
+                    </div>
+                    <div className="item">
+                      <h3>通知音</h3>
+                      <select name="options.soundfile" onChange={this.onChange} value={this.state['options.soundfile']}>
+                        {this.state.selectableList.soundFiles.map((d) =>
+                          <option value={d.path}>{d.text}</option>
+                        )}
+                      </select>
+                      <p className='note'>選択するとサンプル音が鳴ります</p>
+                    </div>
+                    <div className="item">
+                      <h3>通知音のボリューム</h3>
+                      <input type="range" name="options.playsound.volume" value={this.state['options.playsound.volume']} min="0.0" max="1.0" step="0.1" onChange={this.onChange} />
+                      <button className="soundtest" onClick={this.playSound}>音量テスト</button>
+                    </div>
+                  </div>
+                  <div>
+                    <input id="saveAll" type="submit" value="設定を保存する" onClick={this.saveSettings}/>
+                    <p id="console" style={{color: '#228b22'}}>{this.state.resultMessage}</p>
+                  </div>
+                  <div className="wrapper">
+                    <h1 className="appicon">作者にカンパする</h1>
+                    <div className="items">
+                      <span className="campa">にこさぽの開発を応援してくださる方はよろしくお願いします．今後の開発の励みになります．🙏</span>
+                        <p> <a target="_blank" href="http://amzn.asia/hqChgj3">Amazon ほしいものリスト - ほしい雑貨</a></p>
+                        <p> <a target="_blank" href="http://amzn.asia/8BFBccC">Amazon ほしいものリスト - ほしい本</a></p>
+                    </div>
+                  </div>
+                </div>
+              )
+            }
+          })()}
+          {(() => {
+            if (this.state.selectedMenu == 'auto-program') {
+              return(
+                <div className="wrapper">
+                  <h1 className="appicon">自動入場が有効になっている番組</h1>
+                  <div id="listgroup-program">
+                    <AutoEnterList type='community' />
+                  </div>
+                </div>
+              )
+            }
+          })()}
+          {(() => {
+            if (this.state.selectedMenu == 'auto-community') {
+              return(
+                <div className="wrapper">
+                  <h1 className="appicon">自動入場が有効になっているCH・コミュ</h1>
+                  <div id="listgroup-community">
+                    <AutoEnterList type='program' />
+                  </div>
+                </div>
+              )
+            }
+          })()}
         </div>
       </div>
     )
