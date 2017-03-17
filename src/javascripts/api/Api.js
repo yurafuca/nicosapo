9869a19f2220177f94c970193083b0975984c857
tusyuno <madokawaii@gmail.com>
2017-03-17 16:39:35 +0900

    Use VideoInfo.js

--- src/javascripts/api/Api.js
+++ src/javascripts/api/Api.js
@@ -1,4 +1,6 @@
-import $ from 'jquery';
+import $ from 'jquery'
+import { parseString } from 'xml2js'
+import VideoInfo from '../modules/VideoInfo'
 
 const parameter_nicovideojs = [];
 
@@ -22,132 +24,80 @@ export default class Api {
 
   static loadCasts(liveType) {
     return new Promise((resolve, reject) => {
-      if (liveType == 'user') {
-        Api.getSubscribe_2().then(($videoInfos) => {
-          resolve($videoInfos);
-        }).catch(reject);
-      }
-      if (liveType == 'official') {
-        Api.getOfficialOnair().then((official_lives) => {
-          resolve(official_lives);
-        });
-      }
-      if (liveType == 'future') {
+      switch(liveType) {
+        case 'user':
+          Api.getUserOnair().then(($videoInfos) => {
+            console.info($videoInfos);
+            resolve($videoInfos);
+          }).catch(reject);
+          break;
+        case 'official':
+          Api.getOfficialOnair().then((official_lives) => {
+            resolve(official_lives);
+          });
+          break
+        case 'future':
           Api.getFutureOnair().then((future_lives) => {
               resolve(future_lives);
           });
+          break;
       }
     });
   }
 
-  // 有料限定∧公式チャンネル の情報が含まれない
-  static getSubscribe_2() {
+  // jQuery オブジェクトでなく JSON を返したい
+  static getUserOnair() {
     return new Promise((resolve) => {
       const endpoint = 'http://live.nicovideo.jp/my';
       $.post(endpoint, (response) => {
         const $html = $(response);
         const subscribes = $html.find("[id$=subscribeItemsWrap] > .liveItems > [class^='liveItem']");
         const reserved = $html.find("[id$=subscribeReservedItemsWrap] > .liveItems > [class^='liveItem']");
-
         $.each($(reserved), (index, item) => {
           item.is_reserved = true;
         });
-
         $.merge(subscribes, reserved);
-
         const videoInfos = [];
-
         $.each($(subscribes), (index, item) => {
-
           const video_title = $(item).find('a').first().attr('title');
           const video_url = $(item).find('a').first().attr('href');
           const video_id = video_url.match(/(lv\d+)/g)[0];
-
           const img = $($(item).find('img')[0]);
-
           if (img.attr('alt') == 'CLOSED') {
             return;
           }
-
           const community_thumbnail_base = img.attr('src').match(/http\:\/\/icon.nimg.jp\/(channel|community)\//)[0];
           const community_id = img.attr('src').match(/(ch|co)\d+/)[0];
           const community_thumbnail = `${community_thumbnail_base}${community_id}.jpg`;
-
           const video_open_time_jpstr = $(item).find('strong').first().text();
-
-          const videoInfo = $(`
-                        <video_info>
-                            <video>
-                                <id></id>
-                                <title></title>
-                                <open_time></open_time>
-                                <open_time_jpstr></open_time_jpstr>
-                                <start_time></start_time>
-                                <schedule_end_time/>
-                                <end_time></end_time>
-                                <provider_type>y</provider_type>
-                                <related_channel_id/>
-                                <hidescore_online></hidescore_online>
-                                <hidescore_comment></hidescore_comment>
-                                <community_only></community_only>
-                                <channel_only></channel_only>
-                                <view_counter></view_counter>
-                                <comment_count></comment_count>
-                                <is_panorama></is_panorama>
-                                <_ts_reserved_count></_ts_reserved_count>
-                                <timeshift_enabled></timeshift_enabled>
-                                <is_hq></is_hq>
-                                <is_reserved></is_reserved>
-                            </video>
-                            <community>
-                                <id></id>
-                                <name></name>
-                                <channel_id/>
-                                <global_id></global_id>
-                                <thumbnail></thumbnail>
-                                <thumbnail_small></thumbnail_small>
-                            </community>
-                        </video_info>
-                    `);
-
+          const videoInfo = $(VideoInfo.getString());
           videoInfo.find('community thumbnail').text(community_thumbnail);
           videoInfo.find('video title').text(video_title);
           videoInfo.find('video id').text(video_id);
           videoInfo.find('video open_time_jpstr').text(video_open_time_jpstr);
           videoInfo.find('community id').text(community_id.replace(/(co|ch)/, ''));
-
           if (item.is_reserved) {
             videoInfo.find('video is_reserved').text(true);
           } else {
             videoInfo.find('video is_reserved').text(false);
           }
-
           videoInfos.push(videoInfo);
         });
-
         resolve(videoInfos);
       });
-
     });
   }
-
 
   static getCheckList() {
     return new Promise((resolve, reject) => {
       const endpoint = "http://flapi.nicovideo.jp/api/getchecklist";
       const posting = $.get(endpoint);
-      // Why "posting" calls fail(), not done()?
-      // posting.done((data) => {
-      //   console.info(data);
-      // });
       posting.done((data) => {
         const json = JSON.parse(data);
         const status = json.status;
-        let checkList = '';
         switch (status) {
         case "OK":
-          checkList = json.community_id;
-          resolve(checkList);
+          resolve(json.community_id);
           break;
         default:
           reject(new Error('Request failed: status is "fail".'));
@@ -157,7 +107,7 @@ export default class Api {
     });
   }
 
-  static getFutureOnair(index) {
+  static getFutureOnair() {
     return new Promise((resolve) => {
       const endpoint = "http://live.nicovideo.jp/ranking?type=comingsoon&main_provider_type=official";
       const posting = $.get(endpoint);
