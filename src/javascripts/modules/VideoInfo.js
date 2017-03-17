b771ecfce01bdc9222c90cd8b342bf9752f092ac
tusyuno <madokawaii@gmail.com>
2017-03-17 19:24:17 +0900

    Implement VideoInfo.js

--- src/javascripts/modules/VideoInfo.js
+++ src/javascripts/modules/VideoInfo.js
@@ -1,38 +1,29 @@
-export default class VideoInfos {
-  static getString() {
-    return `
-      <video_info>
-        <video>
-          <id></id>
-          <title></title>
-          <open_time></open_time>
-          <open_time_jpstr></open_time_jpstr>
-          <start_time></start_time>
-          <schedule_end_time/>
-          <end_time></end_time>
-          <provider_type>y</provider_type>
-          <related_channel_id/>
-          <hidescore_online></hidescore_online>
-          <hidescore_comment></hidescore_comment>
-          <community_only></community_only>
-          <channel_only></channel_only>
-          <view_counter></view_counter>
-          <comment_count></comment_count>
-          <is_panorama></is_panorama>
-          <_ts_reserved_count></_ts_reserved_count>
-          <timeshift_enabled></timeshift_enabled>
-          <is_hq></is_hq>
-          <is_reserved></is_reserved>
-        </video>
-        <community>
-          <id></id>
-          <name></name>
-          <channel_id/>
-          <global_id></global_id>
-          <thumbnail></thumbnail>
-          <thumbnail_small></thumbnail_small>
-        </community>
-      </video_info>
-    `
+import $ from 'jquery'
+import Video from '../modules/Video'
+import Community from '../modules/Community'
+
+export default class VideoInfo {
+  constructor() {
+    this._video = new Video();
+    this._community = new Community();
+  }
+
+  video() {
+    return this._video;
+  }
+
+  community() {
+    return this._community;
+  }
+
+  xml() {
+    const xml = $(this._xml());
+    xml.append(this._video.xml());
+    xml.append(this._community.xml());
+    return xml;
+  }
+
+  _xml() {
+    return `<video_info></video_info>`;
   }
 }
