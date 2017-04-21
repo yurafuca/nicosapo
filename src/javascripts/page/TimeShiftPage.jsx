03efc7563bfab9810a7ccdd898b4aa6054503f78
tusyuno <madokawaii@gmail.com>
2017-04-21 23:46:13 +0900

    Change putButton to show an especial message

--- src/javascripts/page/TimeShiftPage.jsx
+++ src/javascripts/page/TimeShiftPage.jsx
@@ -1,8 +1,12 @@
+import $ from 'jquery'
 import CastPage from '../page/CastPage'
 
 export default class TimeShiftPage extends CastPage {
   putButton() {
     // TimeShiftPage dosen't need auto-buttons.
+    const $noSupport = $(`<span></span>`);
+    $noSupport.text(`/* タイムシフトでは各種ボタンと情報バーが無効になります */`);
+    $('.meta').append($noSupport);
   }
 
   buildExBar() {
