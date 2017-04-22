b557bc374b7ba76f225dae9cb1befcdc80f1b432
tusyuno <madokawaii@gmail.com>
2017-04-22 19:26:14 +0900

    Move some process related for buttons and exbar to Widgets

--- src/javascripts/components/Widgets.jsx
+++ src/javascripts/components/Widgets.jsx
@@ -1,29 +1,84 @@
 import React from 'react';
-import NewCastChecker from '../modules/NewCastChecker';
+import ReactDOM from 'react-dom';
 import ExBar from '../components/ExBar';
+import NewCastChecker from '../modules/NewCastChecker';
+import AutoRedirectButton from '../buttons/AutoRedirectButton';
+import AutoEnterCommunityButton from '../buttons/AutoEnterCommunityButton';
 
 const _newCastChecker = new NewCastChecker();
 
-/**
- * ExBar と NewCastChecker をまとめて管理する．
- * 将来的には Button も管理対象にするかもしれない．
- * @author tsuyuno
- */
+function insertAfter(newNode, referenceNode) {
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+function prependChild(newNode, parentNode) {
+  parentNode.insertBefore(newNode, parentNode.firstChild);
+}
+
 export default class Widgets extends React.Component {
   constructor(props) {
     super(props);
     this.state = { response: '' };
-    _newCastChecker.setProlongReceiver(this.prolongReceiver.bind(this));
+    _newCastChecker.setProlongReceiver(this._prolongReceiver.bind(this));
     _newCastChecker.run();
   }
 
-  prolongReceiver(response) {
+  putButtons() {
+    const parent = this.props.element4Buttons;
+    const child = document.createElement('div');
+    child.id = 'nicosapo_buttons';
+    switch(this.props.position) {
+      case `APPEND`:
+        parent.appendChild(child);
+        break;
+      case `PREPEND`:
+        prependChild(child, parent);
+        break;
+      case `AFTER`:
+        insertAfter(child, parent);
+        break;
+      case `BEFORE`:
+        // Not supported.
+        break;
+    }
+    const reactDom = this._getButtons();
+    ReactDOM.render(reactDom, child);
+  }
+
+  _getButtons() {
+    const buttonDoms = [];
+    if (this.props.enableARButton) {
+      buttonDoms.push(<AutoRedirectButton notify={this._recieveNotify} />)
+    }
+    if (this.props.enableACButton) {
+      buttonDoms.push(<AutoEnterCommunityButton />)
+    }
+    if (this.props.enableAPButton) {
+      buttonDoms.push(<AutoEnterProgramButton />)
+    }
+    let wrapper = null;
+    if (this.props.requireInline) {
+      wrapper = <div style={{display: 'inline-block'}}>{buttonDoms}</div>;
+    } else {
+      wrapper = <div>{buttonDoms}</div>;
+    }
+    return(wrapper);
+  }
+
+  _prolongReceiver(response) {
     this.setState({ response: response });
   }
 
+  _recieveNotify(isToggledOn) {
+    _newCastChecker.setMode(isToggledOn);
+  }
+
+  componentDidMount() {
+    this.putButtons();
+  }
+
   render() {
-    return(
-      <ExBar response={this.state.response}/>
-    )
+    const exBar = (this.props.enableExBar) ? <ExBar response={this.state.response}/> : null;
+    return(exBar);
   }
 }
