0450d9e1b11578a339e94292dcbfa2fcc09c19b9
tsuyuno <madokawaii@gmail.com>
2017-06-24 05:13:51 +0900

    Add feature to ignore autoEnter when a cast is enabled as autoRedirect

--- src/javascripts/autoEnter/AutoEnterCommunity.js
+++ src/javascripts/autoEnter/AutoEnterCommunity.js
@@ -1,5 +1,6 @@
 import Api from '../api/Api';
 import store from 'store';
+import NiconamaTabs from '../modules/NiconamaTabs'
 
 const _listKey = 'autoEnterCommunityList';
 const _notificationTitle = '自動入場（コミュニティ/チャンネル）';
@@ -7,6 +8,7 @@ const _notificationTitle = '自動入場（コミュニティ/チャンネル）
 // export default class AutoEnterProgram {
 export default class AutoEnterCommunity {
   exec(id) {
+
     Api.isOpen(id).then((response) => {
       let storagedData = {};
       if (store.get(_listKey)) {
@@ -17,6 +19,26 @@ export default class AutoEnterCommunity {
         const lastState = storagedData[response.requestId].state;
         if (lastState === 'offair') {
           storagedData[response.requestId].state = 'onair';
+
+          // 自動次枠移動が有効になっている場合は無視する．
+          const tabId = NiconamaTabs.getTabId(id);
+          if (tabId) {
+            chrome.tabs.sendMessage(Number(tabId), `isEnableChecking`, null, (response) => {
+              console.log(`castId = ${id}, tabId = ${tabId}, autoRedirect = ${response}`);
+              if (response) {
+                const communityData = store.get('autoEnterCommunityList')[id];
+                const options = {
+                  body: `放送が始まりましたが，自動次枠移動が有効になっているため，自動入場をキャンセルしました．`,
+                  icon: communityData.thumbnail
+                };
+                const duration = store.get('options.openingNotification.duration') || 6;
+                const notification = new Notification(communityData.title, options);
+                setTimeout((notification) => { notification.close(); }, duration * 1000);
+                return;
+              }
+            });
+          }
+
           chrome.tabs.create({ url: `http://live.nicovideo.jp/watch/${id}` }, () => {
             const options = {
               type: 'basic',
