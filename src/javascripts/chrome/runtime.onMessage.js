74aa6a8a36d7c1b1a7e62637ff61c30c0653c8de
tsuyuno <madokawaii@gmail.com>
2017-07-05 19:09:28 +0900

    Support for remaining scroll position

--- src/javascripts/chrome/runtime.onMessage.js
+++ src/javascripts/chrome/runtime.onMessage.js
@@ -3,11 +3,15 @@ import NiconamaTabs from '../modules/NiconamaTabs'
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.purpose == 'NiconamaTabs.add') {
-    NiconamaTabs.add(sender.tab.id, request.id);
+    NiconamaTabs.add(sender.tab.id, request.id, request.scrollTop);
   } 
 
   if (request.purpose == 'NiconamaTabs.remove') {
     NiconamaTabs.remove(sender.tab.id);
+  }
+
+  if (request.purpose == 'NiconamaTabs.get') {
+    sendResponse(NiconamaTabs.get(sender.tab.id));
   }
 
   if (request.purpose == 'getFromLocalStorage') {
--- src/javascripts/page/CastPage.js
+++ src/javascripts/page/CastPage.js
@@ -1,13 +1,41 @@
+import $ from 'jquery'
 import IdHolder from '../modules/IdHolder'
 import Page from '../page/Page'
 
 export default class CastPage extends Page {
   constructor() {
     super();
-    console.log('castpage');
-    chrome.runtime.sendMessage({
-      purpose: 'NiconamaTabs.add',
-      id: (new IdHolder()).communityId
+    this.communityId = (new IdHolder()).communityId;
+    
+    /**
+     * 次枠移動を使用した場合はスクロール位置を復元する
+     * そうでなければ NiconamaTabs に登録する
+     */ 
+    chrome.runtime.sendMessage({ purpose: 'NiconamaTabs.get' }, (response) => {
+      if (response == null || response.castId != this.communityId) {
+        chrome.runtime.sendMessage({
+          purpose: 'NiconamaTabs.add',
+          id: this.communityId,
+          scrollTop: response ? response.scrollTop : 0
+        });
+      } else {
+        // ロードを待つ
+        setTimeout(() => {
+          const scrollTop = response.scrollTop;
+          $('html,body').animate({scrollTop: scrollTop}, 200, 'swing');
+        }, 3 * 1000);
+      }
     });
-  }
+
+    /**
+     * スクロール位置を localStorage で保持する
+     */
+    window.addEventListener('scroll', (e) => {
+      chrome.runtime.sendMessage({
+        purpose: 'NiconamaTabs.add',
+        id: this.communityId,
+        scrollTop: document.body.scrollTop
+      });
+    });
+ }
 }
