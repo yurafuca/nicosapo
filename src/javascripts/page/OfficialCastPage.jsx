759a26c11bfea9eb2b118a2bc0080045654cf1fa
amagomori <root@yurafuca.com>
2018-01-08 22:35:45 +0900

    Fix an issue that buttons are displayed when opening old official page

--- src/javascripts/page/OfficialCastPage.jsx
+++ src/javascripts/page/OfficialCastPage.jsx
@@ -2,13 +2,14 @@ import CastPage from '../page/CastPage';
 
 export default class OfficialCastPage extends CastPage {
   putWidgets() {
+      const isRetroPage = document.querySelector("[class^='___toggle-button___']") == null;
       const props = {
         buttonOrder : `MESSAGE`,
         message     : `/* にこさぽ: 公式番組ではでは各種ボタンが無効になります */`,
         position    : `APPEND`,
         enableExBar : true,
-        element4Buttons: document.querySelector("[class^='___toggle-button___']"),
-        idName4ExBar: 'class^="___operator-area___"'
+        element4Buttons: document.querySelector("[class^='___toggle-button___']") || document.getElementById('channel_interface_bookmark'),
+        idName4ExBar: isRetroPage ? 'id=watch_player_top_box' : 'class^="___operator-area___"'
       };
       super.putWidgets(props);
   }
