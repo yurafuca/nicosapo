3dafb7a4e163e025a97d9b781576fb4eac3c3456
amagomori <root@yurafuca.com>
2018-01-10 00:44:50 +0900

    Use ES6

--- test/CastingCommunities_spec.js
+++ test/CastingCommunities_spec.js
@@ -1,13 +1,13 @@
 /*global describe, it, before */
 
-import { expect } from 'chai'
-import { jsdom } from 'jsdom'
-const document = jsdom('<div/');
+import { expect } from "chai";
+import { jsdom } from "jsdom";
+const document = jsdom("<div/");
 const window = document.defaultView;
-import jQuery from 'jquery'
+import jQuery from "jquery";
 const $ = jQuery(window);
-import VideoInfos from '../test/VideoInfos';
-import CastingCommunities from '../src/javascripts/modules/CastingCommunities'
+import VideoInfos from "../test/VideoInfos";
+import CastingCommunities from "../src/javascripts/modules/CastingCommunities";
 
 const castingCommunities = new CastingCommunities();
 const videoInfo = VideoInfos.getString();
@@ -15,60 +15,51 @@ const videoInfo = VideoInfos.getString();
 const videoInfosHash_previous = {};
 const videoInfosHash_current = {};
 
-describe('CastingComunities', function() {
-    before(function(done) {
-      const $vi1 = new $(videoInfo);
-      $vi1.find('video open_time_jpstr').text('time0');
-      $vi1.find('community id').text('000000');
+describe("CastingComunities", () => {
+  before(done => {
+    const $vi1 = new $(videoInfo);
+    $vi1.find("video open_time_jpstr").text("time0");
+    $vi1.find("community id").text("000000");
 
-      const $vi2 = new $(videoInfo);
-      $vi2.find('video open_time_jpstr').text('time1');
-      $vi2.find('community id').text('111111');
+    const $vi2 = new $(videoInfo);
+    $vi2.find("video open_time_jpstr").text("time1");
+    $vi2.find("community id").text("111111");
 
-      const $vi3 = new $(videoInfo);
-      $vi3.find('video open_time_jpstr').text('time2');
-      $vi3.find('community id').text('222222');
+    const $vi3 = new $(videoInfo);
+    $vi3.find("video open_time_jpstr").text("time2");
+    $vi3.find("community id").text("222222");
 
-      const $vi4 = new $(videoInfo);
-      $vi4.find('video open_time_jpstr').text('time3');
-      $vi4.find('community id').text('333333');
+    const $vi4 = new $(videoInfo);
+    $vi4.find("video open_time_jpstr").text("time3");
+    $vi4.find("community id").text("333333");
 
-      const $vi5 = new $(videoInfo);
-      $vi5.find('video open_time_jpstr').text('time4');
-      $vi5.find('community id').text('444444');
+    const $vi5 = new $(videoInfo);
+    $vi5.find("video open_time_jpstr").text("time4");
+    $vi5.find("community id").text("444444");
 
-      videoInfosHash_previous['000000'] = $vi1;
-      videoInfosHash_previous['111111'] = $vi2;
-      videoInfosHash_previous['222222'] = $vi3;
+    videoInfosHash_previous["000000"] = $vi1;
+    videoInfosHash_previous["111111"] = $vi2;
+    videoInfosHash_previous["222222"] = $vi3;
 
-      videoInfosHash_current['000000'] = $vi1;
-      videoInfosHash_current['111111'] = $vi2;
-      videoInfosHash_current['222222'] = $vi3;
-      videoInfosHash_current['333333'] = $vi4;
-      videoInfosHash_current['444444'] = $vi5;
+    videoInfosHash_current["000000"] = $vi1;
+    videoInfosHash_current["111111"] = $vi2;
+    videoInfosHash_current["222222"] = $vi3;
+    videoInfosHash_current["333333"] = $vi4;
+    videoInfosHash_current["444444"] = $vi5;
 
-      castingCommunities.push(videoInfosHash_previous);
-      castingCommunities.push(videoInfosHash_current);
+    castingCommunities.push(videoInfosHash_previous);
+    castingCommunities.push(videoInfosHash_current);
 
-      done();
-    });
-    it('#ALL は現在放送中の id の Object を返却する．', function() {
-      const all = castingCommunities.query('ALL');
-      const ids = Object.keys(all).sort();
-      expect(ids).to.eql([
-        '000000',
-        '111111',
-        '222222',
-        '333333',
-        '444444',
-      ]);
-    });
-    it('#ONLY_JUST_FOLLOWED はフォローしたばかりの id の Array を返却する．', function() {
-      const started_1 = castingCommunities.query('ONLY_JUST_STARTED');
-      const ids = Object.keys(started_1);
-      expect(ids).to.eql([
-        '333333',
-        '444444'
-      ]);
-    });
+    done();
+  });
+  it("#ALL は現在放送中の id の Object を返却する．", () => {
+    const all = castingCommunities.query("ALL");
+    const ids = Object.keys(all).sort();
+    expect(ids).to.eql(["000000", "111111", "222222", "333333", "444444"]);
+  });
+  it("#ONLY_JUST_FOLLOWED はフォローしたばかりの id の Array を返却する．", () => {
+    const started_1 = castingCommunities.query("ONLY_JUST_STARTED");
+    const ids = Object.keys(started_1);
+    expect(ids).to.eql(["333333", "444444"]);
+  });
 });
--- test/FolllwingCommunities_spec.js
+++ test/FolllwingCommunities_spec.js
@@ -1,98 +1,62 @@
 /*global describe, it, before */
 
-import { expect } from 'chai'
-import FollowingCommunities from '../src/javascripts/modules/FollowingCommunities'
+import { expect } from "chai";
+import FollowingCommunities from "../src/javascripts/modules/FollowingCommunities";
 
 const followingComunities = new FollowingCommunities();
 
-describe('FollowingComunities', function() {
-  describe('push回数が1', function() {
-    before(function(done) {
-      followingComunities.push([
-        '000000',
-        '111111',
-        '222222',
-        '333333',
-      ]);
+describe("FollowingComunities", () => {
+  describe("push回数が1", () => {
+    before(done => {
+      followingComunities.push(["000000", "111111", "222222", "333333"]);
       done();
     });
-    it('#ALL は 最新のコミュニティの id の Array を返却する．', function() {
-      const all = followingComunities.query('ALL');
-      expect(all).to.eql([
-        '000000',
-        '111111',
-        '222222',
-        '333333',
-      ]);
+    it("#ALL は 最新のコミュニティの id の Array を返却する．", () => {
+      const all = followingComunities.query("ALL");
+      expect(all).to.eql(["000000", "111111", "222222", "333333"]);
     });
-    it('#ONLY_JUST_FOLLOWED はフォローしたばかりの id の Array を返却する．', function() {
-      const followed_1 = followingComunities.query('ONLY_JUST_FOLLOWED');
-      expect(followed_1).to.eql([
-        '000000',
-        '111111',
-        '222222',
-        '333333',
-      ]);
+    it("#ONLY_JUST_FOLLOWED はフォローしたばかりの id の Array を返却する．", () => {
+      const followed_1 = followingComunities.query("ONLY_JUST_FOLLOWED");
+      expect(followed_1).to.eql(["000000", "111111", "222222", "333333"]);
     });
-    it('#ONLY_JUST_FOLLOWED はフォローしたばかりの id の Array を返却する．', function() {
-      followingComunities.push([
-        '000000',
-        '111111',
-        '666666'
-      ]);
-      const followed_2 = followingComunities.query('ONLY_JUST_FOLLOWED');
-      expect(followed_2).to.eql([
-        '666666',
-      ]);
+    it("#ONLY_JUST_FOLLOWED はフォローしたばかりの id の Array を返却する．", () => {
+      followingComunities.push(["000000", "111111", "666666"]);
+      const followed_2 = followingComunities.query("ONLY_JUST_FOLLOWED");
+      expect(followed_2).to.eql(["666666"]);
     });
   }),
-
-  describe('push回数が2', function() {
-    before(function(done) {
-      followingComunities.push([
-        '000000',
-        '111111',
-        '222222',
-        '333333',
-      ]);
-      followingComunities.push([
-        '000000',
-        '111111',
-        '222222',
-        '333333',
-        '444444',
-        '555555'
-      ]);
-      done();
+    describe("push回数が2", () => {
+      before(done => {
+        followingComunities.push(["000000", "111111", "222222", "333333"]);
+        followingComunities.push([
+          "000000",
+          "111111",
+          "222222",
+          "333333",
+          "444444",
+          "555555"
+        ]);
+        done();
+      });
+      it("#ALL は 最新のコミュニティの id の Array を返却する．", () => {
+        const all = followingComunities.query("ALL");
+        expect(all).to.eql([
+          "000000",
+          "111111",
+          "222222",
+          "333333",
+          "444444",
+          "555555"
+        ]);
+      });
+      it("#ONLY_JUST_FOLLOWED はフォローしたばかりの id の Array を返却する．", () => {
+        const followed_1 = followingComunities.query("ONLY_JUST_FOLLOWED");
+        expect(followed_1).to.eql(["444444", "555555"]);
+      });
+      it("#ONLY_JUST_FOLLOWED はフォローしたばかりの id の Array を返却する．", () => {
+        followingComunities.push(["000000", "111111", "666666"]);
+        const followed_2 = followingComunities.query("ONLY_JUST_FOLLOWED");
+        expect(followed_2).to.eql(["666666"]);
+      });
     });
-    it('#ALL は 最新のコミュニティの id の Array を返却する．', function() {
-      const all = followingComunities.query('ALL');
-      expect(all).to.eql([
-        '000000',
-        '111111',
-        '222222',
-        '333333',
-        '444444',
-        '555555'
-      ]);
-    });
-    it('#ONLY_JUST_FOLLOWED はフォローしたばかりの id の Array を返却する．', function() {
-      const followed_1 = followingComunities.query('ONLY_JUST_FOLLOWED');
-      expect(followed_1).to.eql([
-        '444444',
-        '555555'
-      ]);
-    });
-    it('#ONLY_JUST_FOLLOWED はフォローしたばかりの id の Array を返却する．', function() {
-      followingComunities.push([
-        '000000',
-        '111111',
-        '666666'
-      ]);
-      const followed_2 = followingComunities.query('ONLY_JUST_FOLLOWED');
-      expect(followed_2).to.eql([
-        '666666',
-      ]);
-    });
-  })
 });
