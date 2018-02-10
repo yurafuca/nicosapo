cea1fec0c4da9359ba365613ec396cab29e56259
amagomori <root@yurafuca.com>
2018-02-11 01:59:53 +0900

    Fix bug that buttons are not displayed correctly

--- src/javascripts/page/OfficialCastPage.jsx
+++ src/javascripts/page/OfficialCastPage.jsx
@@ -2,13 +2,16 @@ import CastPage from '../page/CastPage';
 
 export default class OfficialCastPage extends CastPage {
   putWidgets() {
-      const isRetroPage = document.querySelector("[class^='___toggle-button___']") == null;
+      const isRetroPage = document.querySelector("[class^='___toggle-button___']") == null
+                          && document.querySelector("[class^='___provider-detail___']") == null;
       const props = {
         buttonOrder : `MESSAGE`,
-        message     : `/* にこさぽ: 公式番組ではでは各種ボタンが無効になります */`,
+        message     : `/* にこさぽ: ドワンゴ社提供の番組では各種ボタンが無効になります */`,
         position    : `APPEND`,
         enableExBar : true,
-        element4Buttons: document.querySelector("[class^='___toggle-button___']") || document.getElementById('channel_interface_bookmark'),
+        element4Buttons: document.querySelector("[class^='___toggle-button___']") // 新配信
+                         || document.querySelector("[class^='___provider-detail___']") // フォローボタンのない新配信
+                         || document.getElementById('channel_interface_bookmark'), // 旧配信
         idName4ExBar: isRetroPage ? 'id=watch_player_top_box' : 'class^="___operator-area___"'
       };
       super.putWidgets(props);
