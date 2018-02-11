b92209bd327519efa6187abc0f62d79e5bc0fc9c
amagomori <root@yurafuca.com>
2018-02-11 18:37:30 +0900

    Fix bug that follow toggle button will hidden on official cast page

--- src/javascripts/page/OfficialCastPage.jsx
+++ src/javascripts/page/OfficialCastPage.jsx
@@ -2,14 +2,14 @@ import CastPage from '../page/CastPage';
 
 export default class OfficialCastPage extends CastPage {
   putWidgets() {
-      const isRetroPage = document.querySelector("[class^='___toggle-button___']") == null
+      const isRetroPage = document.querySelector("[class^='___follow-toggle-form___']") == null
                           && document.querySelector("[class^='___provider-detail___']") == null;
       const props = {
         buttonOrder : `MESSAGE`,
         message     : `/* にこさぽ: ドワンゴ社提供の番組では各種ボタンが無効になります */`,
         position    : `APPEND`,
         enableExBar : true,
-        element4Buttons: document.querySelector("[class^='___toggle-button___']") // 新配信
+        element4Buttons: document.querySelector("[class^='___follow-toggle-form___']") // 新配信
                          || document.querySelector("[class^='___provider-detail___']") // フォローボタンのない新配信
                          || document.getElementById('channel_interface_bookmark'), // 旧配信
         idName4ExBar: isRetroPage ? 'id=watch_player_top_box' : 'class^="___operator-area___"'
