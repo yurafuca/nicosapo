36933104791a44457b58abe5a476984a49cd17a2
amagomori <root@yurafuca.com>
2018-03-04 05:27:50 +0900

    Fix a bug where tooltips hide when mouse over over date

--- src/javascripts/modules/Thumbnail.js
+++ src/javascripts/modules/Thumbnail.js
@@ -81,7 +81,7 @@ export default class Thumbnail {
       }
     };
 
-    const tooltip = Thumbnail.setTooltip(span, options);
+    const tooltip = Thumbnail.setTooltip(thumbnail, options);
 
     thumbnail.appendChild(outer);
     outer.appendChild(inner);
