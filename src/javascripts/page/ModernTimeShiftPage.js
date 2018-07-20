d96fa17f5800c278be2f7c81fde29cfaf44fefb1
amagomori <root@yurafuca.com>
2018-07-20 19:05:06 +0900

    fix: Fix bug that auto-redirection runs on time shift page

---
+++ src/javascripts/page/ModernTimeShiftPage.js
@@ -0,0 +1,15 @@
+import CastPage from '../page/CastPage'
+
+export default class ModernTimeShiftPage extends CastPage {
+  putWidgets() {
+    const props = {
+      buttonOrder: `MESSAGE`,
+      message: `/* にこさぽ: タイムシフトでは各種ボタンと情報バーが無効になります */`,
+      position: `APPEND`,
+      enableExBar: false,
+      element4Buttons: document.querySelector('[class^="___follow-toggle-form__"]'),
+      idName4ExBar: 'class^="___operator-area___"'
+    };
+    super.putWidgets(props);
+  }
+}
