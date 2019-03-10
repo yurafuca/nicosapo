54e67889fc5cab4fce271d802208517617982309
yurafuca <root@yurafuca.com>
2019-03-10 21:03:11 +0900

    Extend Bucket

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,4 +1,4 @@
-import { Bucket, ProgramBuilder } from "../Bucket"
+import { Bucket, ProgramBuilder } from "../src/javascripts/modules/Bucket"
 
 
 // describe('hello', () => {
@@ -11,35 +11,35 @@ import { Bucket, ProgramBuilder } from "../Bucket"
 describe('builderTest', () => {
     it('plane', () => {
         const builder = new ProgramBuilder();
-        const program = builder.build();
-        expect(program.shouldOpenAutomatically).toEqual(false);
-        expect(program.shouldMoveAutomatically).toEqual(false);
-        expect(program.isVisiting).toEqual(false);
-        expect(program.isJustFollowed).toEqual(false);
-        expect(program.isJustStarted).toEqual(false);
+        const community = builder.buildCommunity();
+        expect(community.shouldOpenAutomatically).toEqual(false);
+        expect(community.shouldMoveAutomatically).toEqual(false);
+        expect(community.isVisiting).toEqual(false);
+        expect(community.isJustFollowed).toEqual(false);
+        expect(community.isJustStarted).toEqual(false);
     });
     it('shouldOpenAutomatically', () => {
         const builder = new ProgramBuilder();
-        const program = builder.shouldOpenAutomatically().build();
-        expect(program.shouldOpenAutomatically).toEqual(true);
+        const community = builder.shouldOpenAutomatically().buildCommunity();
+        expect(community.shouldOpenAutomatically).toEqual(true);
     });
     it('shouldMoveAutomatically', () => {
         const builder = new ProgramBuilder();
-        const program = builder.shouldMoveAutomatically().build();
-        expect(program.shouldMoveAutomatically).toEqual(true);
+        const community = builder.shouldMoveAutomatically().buildCommunity();
+        expect(community.shouldMoveAutomatically).toEqual(true);
     });
     it('visiting', () => {
         const builder = new ProgramBuilder();
-        const program = builder.visiting().build();
-        expect(program.isVisiting).toEqual(true);
+        const community = builder.visiting().buildCommunity();
+        expect(community.isVisiting).toEqual(true);
     });
 });
 
 describe('hello', () => {
     const bucket = new Bucket();
     const builder = new ProgramBuilder();
-    bucket.register(builder.visiting().build());
-    bucket.register(builder.visiting().shouldMoveAutomatically().build());
+    bucket.register(builder.visiting().buildCommunity());
+    bucket.register(builder.visiting().shouldMoveAutomatically().buildCommunity());
     it('...', () => {
         const list = bucket.all().filter(p => p.isVisiting);
         expect(list.length).toEqual(2);
@@ -57,29 +57,41 @@ describe('hello', () => {
 describe('hello', () => {
     const bucket = new Bucket();
     const builder = new ProgramBuilder();
-    const program1 = builder.visiting().build();
-    const program2 = builder.visiting().shouldMoveAutomatically().build();
+    const community1 = builder.visiting().buildCommunity();
+    const community2 = builder.visiting().shouldMoveAutomatically().buildCommunity();
+    bucket.register(community1);
+    bucket.register(community2);
+    it('...', () => {
+        expect(bucket.all().filter(p => p.isVisiting).length).toEqual(2);
+        community1.onCheck();
+        expect(bucket.all().filter(p => p.isVisiting).length).toEqual(2);
+        community2.onLeave();
+        expect(bucket.all().filter(p => p.isVisiting).length).toEqual(1);
+        community2.onVisit();
+        expect(bucket.all().filter(p => p.isVisiting).length).toEqual(2);
+        community2.onFollow();
+        community1.onFollow();
+        community1.onStart();
+        community2.onStart();
+        expect(bucket.all().filter(p => p.isJustFollowed).length).toEqual(2);
+        expect(bucket.all().filter(p => p.isJustStarted).length).toEqual(2);
+        community1.onEnd();
+        community2.onEnd();
+        expect(bucket.all().filter(p => p.isJustStarted).length).toEqual(0);
+        community1.onCheck();
+        community2.onCheck();
+        expect(bucket.all().filter(p => p.isJustFollowed).length).toEqual(0);
+    });
+});
+
+describe('hello', () => {
+    const bucket = new Bucket();
+    const builder = new ProgramBuilder();
+    const program1 = builder.visiting().buildCommunity();
+    const program2 = builder.visiting().shouldMoveAutomatically().buildCommunity();
     bucket.register(program1);
     bucket.register(program2);
     it('...', () => {
-        expect(bucket.all().filter(p => p.isVisiting).length).toEqual(2);
-        program1.onCheck();
-        expect(bucket.all().filter(p => p.isVisiting).length).toEqual(2);
-        program2.onLeave();
-        expect(bucket.all().filter(p => p.isVisiting).length).toEqual(1);
-        program2.onVisit();
-        expect(bucket.all().filter(p => p.isVisiting).length).toEqual(2);
-        program2.onFollow();
-        program1.onFollow();
-        program1.onStart();
-        program2.onStart();
-        expect(bucket.all().filter(p => p.isJustFollowed).length).toEqual(2);
-        expect(bucket.all().filter(p => p.isJustStarted).length).toEqual(2);
-        program1.onEnd();
-        program2.onEnd();
-        expect(bucket.all().filter(p => p.isJustStarted).length).toEqual(0);
-        program1.onCheck();
-        program2.onCheck();
-        expect(bucket.all().filter(p => p.isJustFollowed).length).toEqual(0);
+        bucket.all().filter(p => p.shouldOpenAutomatically)
     });
 });
