92607de3a9277868e5bdf26a98b13ad8f5366b58
yurafuca <root@yurafuca.com>
2019-03-19 21:47:01 +0900

    Add test

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,5 +1,5 @@
 import { Bucket } from "../src/javascripts/modules/Bucket";
-import { CommunityBuilder, ProgramBuilder } from "../src/javascripts/modules/CheckableBuilder";
+import { CommunityBuilder, ProgramBuilder } from "ManageableBuilder.ts";
 
 
 let bucket: Bucket;
@@ -114,9 +114,13 @@ it('take した client は最新の revision を与えられる', () => {
     bucket.mask([c2]);
     expect(bucket.takeProgramsShouldOpen(client1).length).toBe(1);
     expect(bucket.takeProgramsShouldCancelOpen(client2).length).toBe(0);
+    expect(client1.revision()).toBe(1);
+    expect(client2.revision()).toBe(1);
     bucket.touch(c2);
     expect(bucket.takeProgramsShouldOpen(client1).length).toBe(0);
     expect(bucket.takeProgramsShouldCancelOpen(client2).length).toBe(0);
+    expect(client1.revision()).toBe(1);
+    expect(client2.revision()).toBe(1);
 });
 
 it('自動入場が無効なコミュニティと番組を追加したあと mask できる', () => {
@@ -130,8 +134,6 @@ it('自動入場が無効なコミュニティと番組を追加したあと mas
 });
 
 it('自動入場として登録されたばかりの番組は自動入場すべき番組ではない', () => {
-    c1.shouldOpenAutomatically(false);
-    bucket.assign(c1, p1);
     p2.shouldOpenAutomatically(true);
     bucket.appoint(c2, p2);
     expect(bucket.takeProgramsShouldOpen(bucket.createClient()).length).toBe(0);
@@ -264,10 +266,10 @@ it('すでに開始したことがある番組の revision は変更できない
     c1.isFollowing(true);
     bucket.assign(c1, p1);
     bucket.mask([c1]);
-    expect(bucket.programs().filter(p => p.revision() == 0).length).toBe(1);
+    expect(bucket.programs().filter(p => p.revision() == 1).length).toBe(1);
     c1.shouldOpenAutomatically(true);
     bucket.appoint(c1, p1);
-    expect(bucket.programs().filter(p => p.revision() == 0).length).toBe(1);
+    expect(bucket.programs().filter(p => p.revision() == 1).length).toBe(1);
 });
 
 it('Builder の初期化されていないフィールドは null である', () => {
@@ -277,4 +279,173 @@ it('Builder の初期化されていないフィールドは null である', ()
   expect(bucket.programs().filter(p => p.isVisiting).length).toBe(1);
   bucket.assign(c1, new ProgramBuilder().id("lv1"));
   expect(bucket.programs().filter(p => p.isVisiting).length).toBe(1);
+});
+
+it('番組に自動入場するとき shouldOpenAutomatically を false にしなければ入場したにもかかわらず入場をキャンセルしたと通知される', () => {
+    p1.shouldOpenAutomatically(true);
+    p1.isVisiting(true);
+    bucket.assign(c1, p1);
+    expect(bucket.takeProgramsShouldOpen(bucket.createClient()).length).toBe(0);
+    expect(bucket.takeProgramsShouldCancelOpen(bucket.createClient()).length).toBe(1);
+});
+
+it('番組に自動入場するとき shouldOpenAutomatically を false にすれば入場をキャンセルしたと通知されない', () => {
+    p1.shouldOpenAutomatically(true);
+    bucket.assign(c1, p1);
+    bucket.takeProgramsShouldOpen(bucket.createClient()).forEach(p => p.onAutomaticVisit());
+    expect(bucket.takeProgramsShouldOpen(bucket.createClient()).length).toBe(0);
+    expect(bucket.takeProgramsShouldCancelOpen(bucket.createClient()).length).toBe(0);
+});
+
+it('親のない番組を登録できる', () => {
+    p1.shouldOpenAutomatically(true);
+    bucket.appointOrphan(p1);
+    expect(bucket.communityList().filter(c => c.id.startsWith(Bucket.ANONYMOUS_PREFIX)).length).toBe(1);
+});
+
+it('親のない番組にコミュニティを紐づけると，その番組は親のない番組としては appoint できない', () => {
+    bucket.appointOrphan(p1);
+    bucket.assign(c1, p1);
+    p1.shouldOpenAutomatically(true);
+    bucket.appointOrphan(p1);
+    expect(bucket.programs().filter(p => p.community.id.startsWith(Bucket.ANONYMOUS_PREFIX)).length).toBe(0);
+    expect(bucket.programs().filter(p => p.community.id.startsWith(Bucket.ANONYMOUS_PREFIX)).length).toBe(0);
+    expect(bucket.takeProgramsShouldOpen(bucket.createClient()).length).toBe(1);
+});
+
+it('親のない番組にコミュニティを紐づけると，その番組は親のない番組としては assign できない', () => {
+    bucket.assignOrphan(p1);
+    bucket.assign(c1, p1);
+    p1.shouldOpenAutomatically(true);
+    bucket.assignOrphan(p1);
+    expect(bucket.programs().filter(p => p.community.id.startsWith(Bucket.ANONYMOUS_PREFIX)).length).toBe(0);
+    expect(bucket.programs().filter(p => p.community.id.startsWith(Bucket.ANONYMOUS_PREFIX)).length).toBe(0);
+    expect(bucket.takeProgramsShouldOpen(bucket.createClient()).length).toBe(1);
+});
+
+it('親のない番組を複数回 assign しても数が増えない', () => {
+    bucket.appointOrphan(p1);
+    expect(bucket.programs().filter(p => p.community.id.startsWith(Bucket.ANONYMOUS_PREFIX)).length).toBe(1);
+    bucket.appointOrphan(p1);
+    expect(bucket.programs().filter(p => p.community.id.startsWith(Bucket.ANONYMOUS_PREFIX)).length).toBe(1);
+    bucket.appointOrphan(p2);
+    expect(bucket.programs().filter(p => p.community.id.startsWith(Bucket.ANONYMOUS_PREFIX)).length).toBe(2);
+});
+
+it('番組数が 1 以上である親のない番組は削除できない', () => {
+    bucket.appointOrphan(p1);
+    bucket.mask([]);
+    expect(bucket.communityList().filter(c => c.id.startsWith(Bucket.ANONYMOUS_PREFIX)).length).toBe(1);
+});
+
+it('番組数が 0 である親のない番組は削除できる', () => {
+    bucket.appointOrphan(p1);
+    bucket.assign(c1, p1);
+    bucket.mask([]);
+    expect(bucket.communityList().filter(c => c.id.startsWith(Bucket.ANONYMOUS_PREFIX)).length).toBe(0);
+});
+
+it('自動入場した番組は放送開始のおしらせが通知されなくなる', () => {
+    c1.isFollowing(true);
+    p1.shouldOpenAutomatically(true);
+    bucket.assign(c1, p1);
+    bucket.takeProgramsShouldOpen(bucket.createClient()).forEach(p => p.onAutomaticVisit());
+    expect(bucket.takeProgramsShouldNotify(bucket.createClient()).length).toBe(0);
+});
+
+it('開いている番組からコミュニティを自動入場に登録したとき誤って自動入場しない', () => {
+    bucket.assign(c1, p1);
+    c1.shouldOpenAutomatically(true);
+    expect(bucket.takeProgramsShouldOpen(bucket.createClient()).length).toBe(0);
+    expect(bucket.takeProgramsShouldCancelOpen(bucket.createClient()).length).toBe(0);
+    // Open
+    p1.isVisiting(true);
+    p1.isVisited(true);
+    bucket.assign(c1, p1);
+    expect(bucket.takeProgramsShouldOpen(bucket.createClient()).length).toBe(0);
+    expect(bucket.takeProgramsShouldCancelOpen(bucket.createClient()).length).toBe(0);
+});
+
+it('一度自動入場した番組には自動入場しない', () => {
+    c1.shouldOpenAutomatically(true);
+    bucket.assign(c1, p1);
+    expect(bucket.takeProgramsShouldOpen(bucket.createClient()).length).toBe(1);
+    expect(bucket.takeProgramsShouldCancelOpen(bucket.createClient()).length).toBe(0);
+    bucket.takeProgramsShouldOpen(bucket.createClient()).forEach(p => p.onAutomaticVisit());
+    expect(bucket.takeProgramsShouldOpen(bucket.createClient()).length).toBe(0);
+    expect(bucket.takeProgramsShouldCancelOpen(bucket.createClient()).length).toBe(0);
+});
+
+it('一度自動入場した番組はタブを閉じても mask されないし自動入場しない', () => {
+    c1.shouldOpenAutomatically(true);
+    bucket.assign(c1, p1);
+    bucket.assign(c1, p1);
+    // Visit automatically.
+    expect(bucket.takeProgramsShouldOpen(bucket.createClient()).length).toBe(1);
+    bucket.takeProgramsShouldOpen(bucket.createClient()).forEach(p => p.onAutomaticVisit());
+    // Leave.
+    p1clone.isVisiting(false);
+    bucket.assign(c1, p1clone);
+    bucket.mask([]);
+    expect(bucket.takeProgramsShouldOpen(bucket.createClient()).length).toBe(0);
+    expect(bucket.takeProgramsShouldCancelOpen(bucket.createClient()).length).toBe(0);
+});
+
+it('一度開いた番組はタブを閉じても mask されないし自動入場しない', () => {
+    c1.shouldOpenAutomatically(true);
+    bucket.assign(c1, p1);
+    // Visit manually.
+    p1.isVisiting(true);
+    p1.isVisited(true);
+    bucket.assign(c1, p1);
+    // Leave.
+    p1.isVisiting(false);
+    bucket.assign(c1, p1);
+    bucket.mask([]);
+    expect(bucket.takeProgramsShouldOpen(bucket.createClient()).length).toBe(0);
+    expect(bucket.takeProgramsShouldCancelOpen(bucket.createClient()).length).toBe(0);
+});
+
+it('親のない番組を assign できる', () => {
+    p1.shouldOpenAutomatically(true);
+    bucket.assignOrphan(p1);
+    expect(bucket.programsShouldPoll().length).toBe(1);
+    expect(bucket.communitiesShouldPoll().length).toBe(0);
+    expect(bucket.takeProgramsShouldOpen(bucket.createClient()).length).toBe(1);
+    expect(bucket.takeProgramsShouldCancelOpen(bucket.createClient()).length).toBe(0);
+});
+
+it('親のない番組を appoint できる', () => {
+    p1.shouldOpenAutomatically(true);
+    bucket.appointOrphan(p1);
+    expect(bucket.programsShouldPoll().length).toBe(1);
+    expect(bucket.takeProgramsShouldOpen(bucket.createClient()).length).toBe(0);
+    expect(bucket.takeProgramsShouldCancelOpen(bucket.createClient()).length).toBe(0);
+});
+
+it('親のない番組をサムネイルつきで assign できる', () => {
+    const thumbnailUrl = "test.jpg";
+    bucket.assignOrphan(p1, thumbnailUrl);
+    expect(bucket.communityList().filter(c => {
+        return c.id.startsWith(Bucket.ANONYMOUS_PREFIX) && c.thumbnailUrl == thumbnailUrl
+    }).length).toBe(1);
+});
+
+it('親のない番組をサムネイルつきで appoint できる', () => {
+    const thumbnailUrl = "test.jpg";
+    bucket.appointOrphan(p1, thumbnailUrl);
+    expect(bucket.communityList().filter(c => {
+        return c.id.startsWith(Bucket.ANONYMOUS_PREFIX) && c.thumbnailUrl == thumbnailUrl
+    }).length).toBe(1);
+});
+
+it('親のない番組として登録された番組に親を assign できる', () => {
+    p1.shouldOpenAutomatically(true);
+    bucket.assignOrphan(p1);
+    expect(bucket.communityList().filter(c => c.id.startsWith(Bucket.ANONYMOUS_PREFIX)).length).toBe(1);
+    expect(bucket.communityList().filter(c => !c.id.startsWith(Bucket.ANONYMOUS_PREFIX)).length).toBe(0);
+    bucket.assign(c1, p1);
+    bucket.mask([]);
+    expect(bucket.communityList().filter(c => c.id.startsWith(Bucket.ANONYMOUS_PREFIX)).length).toBe(0);
+    expect(bucket.communityList().filter(c => !c.id.startsWith(Bucket.ANONYMOUS_PREFIX)).length).toBe(1);
 });
