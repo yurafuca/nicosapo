3e51600438fd49608c98f78867abbb5bba1413bd
yurafuca <root@yurafuca.com>
2024-09-17 21:38:01 +0900

    予約タブのユーザサムネイルが表示されない問題に対応

--- src/javascripts/modules/FollowApiResponseParser.js
+++ src/javascripts/modules/FollowApiResponseParser.js
@@ -9,10 +9,17 @@ export default class FollowApiResponseParser {
     videoInfo.video().set("openTimeJp", program.beginAt / 1000);
     // フォロー中 API から取得できるかつ放送前であれば予約中である.
     videoInfo.video().set("isReserved", program.liveCycle === "RELEASED");
-
-    videoInfo.community().set("id", program.programProvider.id);
-    videoInfo.community().set("thumbnail", program.programProvider.icon);
+    videoInfo.community().set("id", FollowApiResponseParser.getId(program));
+    videoInfo.community().set("thumbnail", FollowApiResponseParser.getThumbnail(program));
 
     return videoInfo.xml();
+  }
+
+  static getId(program) {
+    return program.socialGroup.id === "co0" ? program.programProvider.id : program.socialGroup.id;
+  }
+
+  static getThumbnail(program) {
+    return program.socialGroup.id === "co0" ? program.programProvider.icon : program.socialGroup.thumbnailUrl;
   }
 }
